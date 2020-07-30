1649156b188e43ad5231644300b7090f6ea1f4d8
kapilsadhwani <kapil.sadhwani@gmail.com>
2020-07-30 10:52:47 -0700

    Testing commit

--- microservice.go
+++ microservice.go
@@ -27,5 +27,5 @@ func port() string {
 
 func index(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Welcome to Cloud Native Go (Update).")
+	fmt.Fprintf(w, "Welcome to Cloud Native Go")
 }
