0a313faae5736468a911e40a3f413b9449987e19
Sunil Sadhwani <sadhwani.sunil@gmail.com>
2020-07-31 16:29:47 -0600

    Initialized Terraform Setup | P:Kapil

---
+++ main.tf
@@ -0,0 +1,39 @@
+# Terraform configuration
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.21.0"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.vpc_azs
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
+
+  enable_nat_gateway = var.vpc_enable_nat_gateway
+
+  tags = var.vpc_tags
+}
+
+module "ec2_instances" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "2.12.0"
+
+  name           = "RESTAPIServer"
+  instance_count = 1
+
+  ami                    = "ami-0323c3dd2da7fb37d"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "stage"
+  }
+}
